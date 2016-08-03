ef6179cab69864399ed2878d53db5cecdfad2f98
Christian <christianvaldez36@hotmail.com>
2016-08-03 09:29:13 -0700

    Added function getContactInfo

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ var counter = 0;
 $(document).ready(function() {
     $("#header h1").click(function() {
         counter++;
-        if(counter == 5) { 
+        if(counter == 5) {
             $("#header h1").fadeOut("slow");
         }
     });
 });
+
+function getContactInfo() {
+    var header = document.getElementById("contactInfo");
+    var info = header.textContent;
+    console.log(info);
+}
+
+getContactInfo();
