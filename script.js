8c0cb48bd084f6dd72ce743e772ffe3872f1c5dc
Christian <christianvaldez36@hotmail.com>
2016-10-21 15:12:54 -0700

    Testing changed the navbar on scroll

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ $(document).ready(function() {
         }
     });
 });
+
+var header_links = document.getElementById("navbarheader"),
+    blue_area = document.getElementById("about");
+
+// Detect on scroll
+window.onscroll = function(e){
+    var startPos = ((e.pageY||document.body.scrollTop) + header_links.offsetHeight);
+    var endPos = blue_area.offsetTop + blue_area.offsetHeight;
+
+    if(blue_area.offsetTop <= startPos && startPos <= endPos){
+        console.log("Should turn a darker color");
+    }else{
+        console.log("Should be a lighter color");
+    }
+};
