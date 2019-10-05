0b15694c7fba3ee88d86c0846bbf294c69350aa4
Christian Valdez <christianvaldez36@hotmail.com>
2019-10-05 10:01:15 -0700

    added script to deploy site

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# Copy CNAME to dist folder
+cp ./CNAME ./dist
+
+# Copy README to dist folder
+cp ./README.md ./dist
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:unChartedZone/unChartedZone.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
