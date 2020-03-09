362d814dcd77553970021a884936684e5dc6de04
Christian Valdez <christianvaldez36@hotmail.com>
2020-03-08 18:13:47 -0700

    realigned footer contents

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,28 @@
 <template>
-	<div id="contact" class="container">
+	<div class="contact container d-flex flex-column align-items-center justify-content-center">
 		<h1 class="section-header">Contact Me!</h1>
 		<span class="contact-item"><i class="fas fa-envelope"></i> Email: <a href="mailto:christianbvaldez36@gmail.com">christianbvaldez36@gmail.com</a></span>
 		<span class="contact-item"><i class="fas fa-phone"></i> Phone: <a href="tel:+1-619-251-7863">619-251-7863</a></span>
 	</div>
 </template>
 
-<style scoped>
-#contact {
-	padding: 180px 20px;
-}
+<style scoped lang="scss">
+.contact {
+	height: 100vh;
 
-.contact-item {
+	&-item {
 	display: block;
 	font-size: 1.5rem;
 	padding-bottom: 20px;
+}
+
+	// Small devices (landscape phones, 576px and up)
+	@media (max-width: 575.98px) { 
+		justify-content: start;
+
+		.section-header {
+			font-size: 3.25rem;
+		}
+	 }
 }
 </style>
