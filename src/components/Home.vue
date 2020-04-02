8a9f6865d8872ba978aee1a2e7749d9696d75c7d
Christian Valdez <christianvaldez36@hotmail.com>
2020-04-01 22:41:27 -0700

    starting over on the styles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,76 +1,12 @@
 <template>
-<div class="header">
-	<div class="header-bg"></div>
-	<div class="header-content d-flex flex-column align-items-center justify-content-center">
-		<h1 class="header-title">Christian Valdez</h1>
-		<h3 class="header-subtitle">California Based Developer</h3>
-	</div>
+<div>
+	<h1>Christian Valdez</h1>
 </div>
 </template>
 
 <script>
 export default {
     data() {
-        return {
-            name: 'Christian Valdez',
-            typewriter: 'California Based Developer',
-        }
     },
-    methods: {
-        temp() {
-            console.log('hello world')
-        }
-    },
-    created() {
-        // window.addEventListener('scroll', this.temp)
-    }
 }
 </script>
-
-<style lang="scss" scoped>
-.header {
-	background-color: black;
-	color: white;
-	position: relative;
-	height: 100vh;
-
-	&-bg {
-		background: url("/images/desk-bg.jpg");
-		background-size: cover;
-		filter: blur(4px);
-		font-family: 'Google Sans';
-		width: 100%;
-		height: 100vh;
-		position: fixed;
-		top: 0;
-		z-index: 4;
-		overflow: hidden;
-	}
-
-	&-content {
-		height: 100vh;
-		position: fixed;
-		z-index: 5;
-		width: 100%;
-	}
-
-	&-title {
-		font-size: 6.25em;
-		padding: 1rem;
-		animation: slideDown .5s ease-out;
-	}
-}
-
-
-/* Extra small devices (portrait phones, less than 576px) */
-@media (max-width: 575.98px) { 
-	.header-title {
-		font-size: 4.25em;
-		text-align: center;
-	}
-
-	.header-subtitle {
-		font-size: 1.5em;
-	}
-}
-</style>
