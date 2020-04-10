93087375711ba3015c1700d1e9957924afdff32d
Christian Valdez <christianvaldez36@hotmail.com>
2020-04-09 23:45:06 -0700

    adjusted colors on main header text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,15 @@
 <template>
 <section class="home-page container d-flex align-start justify-center col">
-	<h2 class="subtitle">Hello 👋, I'm</h2>
-	<h1 class="header-primary">Christian Valdez</h1>
+	<h2 class="header-primary">Hello 👋,</h2>
+	<h1 class="header-primary">
+		<span>I'm</span>
+		<span class="name">Christian Valdez</span>
+	</h1>
 	<h2 class="subtitle">California Based Developer</h2>
-	<button class="home-btn btn btn-primary my-1">
-		<i class="fa fa-chevron-circle-down" />
+	<a href="#" class="home-btn btn btn-primary my-1">
 		About Me
-	</button>
+		<i class="fa fa-chevron-circle-down" />
+	</a>
 </section>
 </template>
 
@@ -21,6 +24,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+i {
+	padding: 0 0.5rem;
+}
+
 .subtitle {
 	animation: slide-in-right ease-out .7s;
 }
@@ -31,10 +39,14 @@ export default {
 	}
 }
 
-.header-primary {
+.name {
 	background-image: linear-gradient(to right, $color-primary, $color-primary-light);
 	-webkit-background-clip: text;
 	color: transparent;
+	padding: 0 2rem;
+}
+
+.header-primary {
 	animation: slide-in-left ease-out .7s;
 }
 
