2df2d812d68c70142b7a42ad8fa091be8ad7c138
Christian Valdez <christianvaldez36@hotmail.com>
2020-04-09 23:45:20 -0700

    added more markup to about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,10 +2,24 @@
 <section class="about container">
 	<h1 class="header-secondary mb-1">About Me</h1>
 	<div class="about-text">
+
 		<p class="paragraph">
-			I am a full stack developer from San Diego, California. I have a
-			Bachelor's Degree in <span class="highlight">Computer Science</span> from SDSU.
+			I am a full stack developer from San Diego, California. I earned
+			my Bachelor's Degree in <span class="highlight">Computer
+			Science</span> from SDSU in 2018.
 		</p>
+
+		<p class="paragraph">
+			There I learned important skills such as data structures, dynamic
+			algorithms, and the intricaies of operating systems
+		</p>
+
+		<p class="paragraph">
+			However for web development. I am completely self taught.
+			Starting with the basics of HTML, CSS, and Javascript, I later
+			moved onto other UI tools such as Jquery and Bootstrap.
+		</p>
+
 	</div>
 </section>
 </template>
@@ -24,6 +38,10 @@ export default {
 
 	&-text {
 		width: 45%;
+
+		p:not(:last-child) {
+			margin: 0 0 2rem 0;
+		}
 	}
 }
 </style>
