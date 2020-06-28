0e8c5772e2da781ce44ccb761e89d6e2024e0c56
Christian Valdez <christianvaldez36@hotmail.com>
2020-06-27 23:53:36 -0700

    Added 2 projects to portfolio
    - ice cream project is using portfolio screenshot
    - descriptions aren't completed yet

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,10 +1,51 @@
 <template>
   <section id="portfolio" class="portfolio-section my-4">
+    <div class="container d-flex col justify-center">
+      <h2 class="header-secondary text-center">Portfolio</h2>
+      <div
+        class="portfolio__item"
+        v-for="project in projects"
+        :key="project.title"
+      >
+        <div class="portfolio__header">
+          <h1>
+            <span class="portfolio__title">{{ project.title }}</span>
+          </h1>
+        </div>
+        <div class="portfolio__content">
+          <div class="portfolio__image">
+            <img :src="`${project.imgSrc}`" alt="" />
+          </div>
+          <!-- <img
+              src="@/assets/images/portfolioScreenshot.png"
+              alt="Screenshot of Portfolio"
+            /> -->
+          <div class="portfolio__body">
+            <p class="portfolio__copy">{{ project.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style lang="scss" scoped>
-	.container {
-		text-align: center;
-		margin: 0 auto 20rem;
-	}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: 'My Portfolio!',
+          description: "This very website you're on right now!",
+          imgSrc: require('@/assets/images/portfolioScreenshot.png')
+        },
+        {
+          title: 'Ice Cream Buisness Management System',
+          description: 'More details coming soon!',
+          imgSrc: require('@/assets/images/portfolioScreenshot.png')
+        }
+      ]
+    }
+  }
+}
+</script>
