138c3249237b580d910860ca5585c422d68564db
Christian Valdez <christianvaldez36@hotmail.com>
2020-07-27 23:48:12 -0700

    added link component

---
+++ src/components/general/ZLink.vue
@@ -0,0 +1,54 @@
+<template>
+  <a :href="url" class="link" :class="{ 'link--outlined': outlined }">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      default: '#',
+    },
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.link {
+  display: inline-block;
+  background-color: $color-primary;
+  color: white;
+  border-radius: 25px;
+  padding: 1.152rem 2.678rem;
+  font-size: 1rem;
+  font-weight: 700;
+  letter-spacing: 1.5px;
+  cursor: pointer;
+  text-transform: uppercase;
+  transition: all 0.2s ease-in-out;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+
+  &,
+  &:active,
+  &:focus,
+  &:visited {
+    text-decoration: none;
+  }
+
+  &:hover {
+    transform: translateY(-0.3rem);
+  }
+
+  &--outlined {
+    border: 1px solid $color-primary;
+    background: transparent;
+    color: $color-primary;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import store from './store'
 import '@fortawesome/fontawesome-free/css/all.css'
 
+import ZLink from './components/general/ZLink'
+
 Vue.config.productionTip = false
+
+Vue.component('z-link', ZLink)
 
 new Vue({
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
