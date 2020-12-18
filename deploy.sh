921d01c4081d9cfcfc9075b40b8a679e9830bee9
Christian Valdez <christianvaldez36@hotmail.com>
2020-12-17 17:40:15 -0800

    changed deploy to be gh-pages branch instead

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:unChartedZone/unChartedZone.github.io.git master
+git push -f git@github.com:unChartedZone/unChartedZone.github.io.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
