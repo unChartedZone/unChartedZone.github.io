d16c45d12ddd02a4ba810d62c3eb4a25af701a7b
Christian Valdez <christianvaldez36@hotmail.com>
2021-01-25 21:26:59 -0800

    removed graphql plugin setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,3 @@
-console.log(__dirname);
-
 module.exports = {
   siteMetadata: {
     title: `Christian Valdez`,
@@ -8,18 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-sass`,
-    {
-      resolve: 'gatsby-source-graphql',
-      options: {
-        typeName: 'yelp',
-        fieldName: 'yelp',
-        url: 'https://api.yelp.com/v3/graphql',
-        headers: {
-          Authorization:
-            'Bearer HWWQejjo7oR39quvor1S88OxzSiwMrtkrHU5NRwtirE7UXTgG8o-2jl7vml6B1gW16nngi6UbNQ67U-A5dkDz2l9UhUxPVEYkID86U1ppqf4_p-I3JUGTzBRdPN3X3Yx',
-        },
-      },
-    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
