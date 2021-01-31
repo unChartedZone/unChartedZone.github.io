712df5edae022910c89b05359fc7ef36d8d86a2c
Christian Valdez <christianvaldez36@hotmail.com>
2021-01-30 20:38:41 -0800

    updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -7,21 +7,21 @@ const About = () => {
       <div className="about-text">
         <p className="paragraph">
           I am a full stack developer from San Diego, California. I earned my
-          Bachelor's Degree in{' '}
-          <span className="highlight">Computer Science</span> from SDSU in 2018.
-          There I learned important skills such as data structures, dynamic
-          algorithms, and the complexities of operating systems. I also learned
-          the fundamentals of Object-oriented programming languages such as{' '}
-          <span className="highlight">Java</span> and{' '}
-          <span className="highlight">C++.</span>
+          Bachelor's Degree in{' '} <span className="highlight">Computer
+          Science</span> from SDSU in 2018.  There I learned important skills
+          such as data structures, dynamic algorithms, and the complexities of
+          operating systems. I learned the fundamentals of Object-oriented
+          programming languages such as  <span className="highlight">Java</span>
+          {' '}and <span className="highlight">C++.</span>
         </p>
 
         <p className="paragraph">
           Meanwhile in school, I wanted to learn about web and mobile
-          development however, there were no classNamees offered at my school.
-          So I decided to teach myself. Starting with the basics of HTML, CSS,
-          and Javascript, I later moved onto other UI frameworks such as Jquery
-          and Bootstrap.
+          development however, there were no classes on those topics offered at
+          my school. So I decided to teach myself. Starting with the basics of
+          HTML, CSS, and Javascript, After mastering those, I moved to learning
+          Jquery and Bootstrap. For the backend, I initially started learning
+          PHP and MySQL.
         </p>
       </div>
     </section>
