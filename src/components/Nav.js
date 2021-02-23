9c828eb054d4c974d6a482242e381fd879bf906d
Christian Valdez <christianvaldez36@hotmail.com>
2021-02-22 16:59:15 -0800

    added toggle animation to navbar hamburger menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Nav = ({ scrollJumper }) => (
-  <nav className="nav d-flex align-center w-full">
-    <div className="nav__main container mx-auto d-flex align-center">
-      <div className="nav-item nav-logo">CV</div>
+const Nav = ({ scrollJumper }) => {
+  let [navToggle, setNavToggle] = useState(false);
 
-      <div className="nav__toggle">&nbsp;</div>
+  const toggleNavMenu = () => {
+    setNavToggle(!navToggle);
+  };
 
-      <ul className="nav-item nav-links">
-        <li className="nav-link" onClick={() => scrollJumper('about')}>
-          <a>About Me</a>
-        </li>
-        <li className="nav-link" onClick={() => scrollJumper('portfolio')}>
-          <a>Portfolio</a>
-        </li>
-        <li className="nav-link" onClick={() => scrollJumper('contact')}>
-          <a>Contact</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+  return (
+    <nav className="nav d-flex align-center w-full">
+      <div className="nav__main container mx-auto d-flex align-center">
+        <div className="nav-item nav-logo">CV</div>
+
+        <div
+          onClick={toggleNavMenu}
+          className={`nav__toggle ${navToggle ? 'nav__toggle--close' : ''}`}
+        >
+          &nbsp;
+        </div>
+
+        <ul
+          className={`nav-item nav-links ${navToggle ? 'nav-links--show' : ''}`}
+        >
+          <li className="nav-link" onClick={() => scrollJumper('about')}>
+            <a>About Me</a>
+          </li>
+          <li className="nav-link" onClick={() => scrollJumper('portfolio')}>
+            <a>Portfolio</a>
+          </li>
+          <li className="nav-link" onClick={() => scrollJumper('contact')}>
+            <a>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
