776e6362d8c646d7011e879e311315d93e93fc2a
Christian Valdez <christianvaldez36@hotmail.com>
2024-08-19 23:09:20 -0700

    updated about me section

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,15 @@ const About = () => {
     <section id="about" className="about container">
       <h1 className="header-secondary mb-1">About Me</h1>
       <div className="about-text">
-        <p className="paragraph"></p>
-
         <p className="paragraph">
-          When I'm away from my keyboard, I'm usually hiking, reading, and
-          drinking coffee.
+          Back in 2011, I decided to enroll in my high school's introductory
+          computer science class. Fast-forward to today, and I've had the
+          privilege of building software for a start-up and a government agency.
+          My main focus these days is building full stack web applications. I
+          most enjoy building software in the sweet spot where design and
+          engineering meet — things that look good but are also built well under
+          the hood. When I'm away from my keyboard, I'm usually hiking, reading,
+          and taking care of my cats.
         </p>
       </div>
     </section>
