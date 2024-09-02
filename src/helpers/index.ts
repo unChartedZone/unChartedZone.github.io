cd961dedb30fe5e8a7f531be17d4084a0fba69c2
Christian Valdez <christianvaldez36@hotmail.com>
2024-09-01 18:37:09 -0700

    moved helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,9 @@
-const scrollJumper = id => {
+const scrollJumper = (id: string) => {
   let offset = 90; // Size of navbar + 20 pixels for a little padding
   let section = document.getElementById(id);
-  window.scrollTo(0, section.offsetTop - offset);
+  if (!!section) {
+    window.scrollTo(0, section.offsetTop - offset);
+  }
 };
 
 export { scrollJumper };
