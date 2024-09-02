b27ac3da6cbe083031b9bad206d11d8f3d15768e
Christian Valdez <christianvaldez36@hotmail.com>
2024-09-01 18:32:15 -0700

    moved over styles to next code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/app/_styles")],
+  },
+};
 
 export default nextConfig;
