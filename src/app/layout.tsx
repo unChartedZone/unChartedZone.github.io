7d4e2898790be74f016077d675a96b4fcb98de4e
Christian Valdez <christianvaldez36@hotmail.com>
2025-01-08 11:04:22 -0800

    added google analytics tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import Nav from "./_components/Nav";
 
@@ -17,6 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Google tag (gtag.js) */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-FNKY85FE8C"
+      ></Script>
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FNKY85FE8C');
+        `}
+      </Script>
       <body className={inter.className}>
         <Nav />
         {children}
